2c1afbb0ad67d23f1cb5c3e11092205dafbf1b38
oghali <oussema.ghali@soprasteria.com>
2021-10-14 17:07:57 +0200

    hotfix: remove language cron

--- translation-app-assessment/backend/reporting/src/index.ts
+++ translation-app-assessment/backend/reporting/src/index.ts
@@ -1,7 +1,6 @@
 import {https} from 'firebase-functions';
-import { launchCron } from './graphql/data/kpi';
+import {launchCron} from './graphql/data/kpi';
 import gqlServer from './graphql/server';
-import {launchCronForLanguages} from "./graphql/data/languages";
 
 
 const server = gqlServer();
@@ -9,5 +8,4 @@ const server = gqlServer();
 const api = https.onRequest(server);
 
 launchCron().then(_=> console.log("kpi created")).catch(err=> console.log("An error has occured by ",err))
-launchCronForLanguages().then(_=> console.log("languages created")).catch(err=> console.log("An error has occured by ",err))
 export {api};
