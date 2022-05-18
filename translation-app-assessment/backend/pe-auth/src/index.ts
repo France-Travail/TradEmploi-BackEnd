dfe5dd971eec3b189e3e73994ea970ac1989ec91
anis <anishezam@gmail.com>
2022-05-18 13:34:57 +0200

    fix etab

--- translation-app-assessment/backend/pe-auth/src/index.ts
+++ translation-app-assessment/backend/pe-auth/src/index.ts
@@ -5,28 +5,32 @@ import { getAccessToken } from "./utils/getAccessToken"
 import { getTokenInfo, isTokenValid } from "./utils/getTokenInfo"
 import { getUserInfo } from "./utils/getUserInfo"
 require("dotenv").config()
-const cors = require('cors')
+const cors = require("cors")
 const app: Express = express()
 const port = 8080
 
 // CORS options
 const corsOptions = {
   origin: /https:\/\/[a-z0-9\-.]*pole-emploi[a-z0-9\-.]+/,
-   //origin: [/https:\/\/[a-z0-9\-.]*pole-emploi[a-z0-9\-.]+/, 'http://localhost:4200'], ///enable localhost during dev
-  methods: ['GET', 'POST'],
-  maxAge: 3600
+  //origin: [/https:\/\/[a-z0-9\-.]*pole-emploi[a-z0-9\-.]+/, 'http://localhost:4200'], ///enable localhost during dev
+  methods: ["GET", "POST"],
+  maxAge: 3600,
 }
 app.use(cors(corsOptions))
-app.disable('x-powered-by')
-
+app.disable("x-powered-by")
 
 app.get("/callback", async (req: Request, res: Response) => {
+  const host = req.get("host")
+  const redirectFront =
+    host === process.env.HOST_ETAB
+      ? process.env.REDIRECT_URI_FRONT_ETAB
+      : process.env.REDIRECT_URI_FRONT
   const codeResp: callBackResponse = req.query as unknown as callBackResponse
   const accessToken: accessTokenResponse | void = await getAccessToken(codeResp)
   if (accessToken) {
     res.redirect(
       302,
-      `${process.env.REDIRECT_URI_FRONT}?access_token=${accessToken.access_token}&state=${codeResp.state}`
+      `${redirectFront}?access_token=${accessToken.access_token}&state=${codeResp.state}`
     )
   } else {
     res.status(501).send("Internal error")
@@ -36,11 +40,13 @@ app.get("/callback", async (req: Request, res: Response) => {
 app.get("/tokeninfo", async (req: Request, res: Response) => {
   const accessToken: string = req.query.access_token as unknown as string
   const tokenInfo = await getTokenInfo(accessToken)
-  if(isTokenValid(tokenInfo.expires_in, tokenInfo.client_id)){
+  if (isTokenValid(tokenInfo.expires_in, tokenInfo.client_id)) {
     const userInfos = await getUserInfo(accessToken)
     res.send(userInfos)
-  }else{
-    res.send("The access token provided is expired, revoked, malformed, or invalid for other reasons.")
+  } else {
+    res.send(
+      "The access token provided is expired, revoked, malformed, or invalid for other reasons."
+    )
   }
 })
 
