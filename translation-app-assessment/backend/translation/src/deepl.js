e38829f8bb4594929fff92cc8f7f45c808efea5c
anis <anishezam@gmail.com>
2022-06-01 14:20:04 +0200

    add logs

--- translation-app-assessment/backend/translation/src/deepl.js
+++ translation-app-assessment/backend/translation/src/deepl.js
@@ -1,6 +1,7 @@
 const axios = require("axios")
 require("dotenv").config()
 const translateWithDeepl = (text, targetLang, sourceLang) => {
+  console.log("translateWithDeepl", text)
   return axios
     .get(
       `https://api-free.deepl.com/v2/translate?auth_key=${process.env.DEEPL_API_KEY}&text=${text}&source_lang=${sourceLang}&target_lang=${targetLang}`
