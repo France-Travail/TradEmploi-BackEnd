4efeaefed2d814f879fdca17c2da1f60f91e825f
anis <anishezam@gmail.com>
2022-06-09 14:42:06 +0200

    fix deepl uri encoding

--- translation-app-assessment/backend/translation/src/deepl.js
+++ translation-app-assessment/backend/translation/src/deepl.js
@@ -1,13 +1,21 @@
 const axios = require("axios")
+
 require("dotenv").config()
 const translateWithDeepl = (text, targetLang, sourceLang) => {
   console.log("translateWithDeepl", text)
   return axios
     .get(
-      `https://api-free.deepl.com/v2/translate?auth_key=${process.env.DEEPL_API_KEY}&text=${text}&source_lang=${sourceLang}&target_lang=${targetLang}`
+      `https://api-free.deepl.com/v2/translate?auth_key=${
+        process.env.DEEPL_API_KEY
+      }&text=${encodeURI(
+        text
+      )}&source_lang=${sourceLang}&target_lang=${targetLang}`
     )
     .then((response) => {
       return response.data.translations[0].text
+    })
+    .catch((error) => {
+      console.error(error)
     })
 }
 
