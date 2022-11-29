5e7f058d45f31fe8ae4f2ea383faa9a635ccf0fa
EOGH2300 <oussema.ghali-ext@pole-emploi.fr>
2022-11-29 10:46:26 +0100

    delete outputfile

--- translation-app-assessment/backend/detect-text/src/ocr.js
+++ translation-app-assessment/backend/detect-text/src/ocr.js
@@ -1,19 +1,49 @@
 const vision = require("@google-cloud/vision")
 const client = new vision.ImageAnnotatorClient()
-const readFile = require("./bucketOperations.js").readFile
+const {readFile, deleteFile} = require("./bucketOperations.js")
 
 async function textDetectionFromPdf(fileName, bucketName) {
-  const result = await readFile(bucketName, fileName)
-  return JSON.parse(result).responses[0].fullTextAnnotation.text
+    const inputConfig = {
+        mimeType: 'application/pdf',
+        gcsSource: {
+            uri: `gs://${bucketName}/${fileName}`,
+        },
+    };
+
+
+    const outputConfig = {
+        gcsDestination: {
+            uri: `gs://${bucketName}/`,
+        },
+    };
+
+    const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
+    const request = {
+        requests: [
+            {
+                inputConfig: inputConfig,
+                features: features,
+                outputConfig: outputConfig
+            },
+        ],
+    };
+
+    const [operation] = await client.asyncBatchAnnotateFiles(request);
+    await operation.promise();
+    const outputFile = 'output-1-to-1.json';
+    const result = await readFile(bucketName, outputFile);
+    deleteFile(outputFile, bucketName);
+    return JSON.parse(result).responses[0].fullTextAnnotation.text;
 }
 
 async function textDetectionFromImage(fileName, bucketName) {
-  const [result] = await client.textDetection(`gs://${bucketName}/${fileName}`)
-  const [annotation] = result.textAnnotations
-  const text = annotation ? annotation.description.trim() : ""
-  return text
+    const [result] = await client.textDetection(`gs://${bucketName}/${fileName}`)
+    const [annotation] = result.textAnnotations
+    const text = annotation ? annotation.description.trim() : ""
+    return text
 }
+
 module.exports = {
-  textDetectionFromPdf,
-  textDetectionFromImage,
+    textDetectionFromPdf,
+    textDetectionFromImage,
 }
