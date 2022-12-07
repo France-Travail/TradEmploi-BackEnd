420e202e9decf9cfd6090f8e7f1d687d6caecedc
anis <hamza-anis.hezam@pole-emploi.fr>
2022-12-07 12:17:54 +0000

    Resolve TRAD-530

---
+++ translation-app-assessment/backend/detect-text/index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const { textDetectionFromImage, textDetectionFromPdf } = require("./src/ocr.js")
+const { uploadFileToBucket, deleteFile } = require("./src/bucketOperations.js")
+
+const app = express()
+app.disable("x-powered-by")
+app.use(bodyParser.json({ limit: "50mb" }))
+const corsOptions = {
+  methods: ["POST"],
+  maxAge: 3600,
+}
+app.use(cors(corsOptions))
+
+const port = process.env.PORT || 8080
+
+app.post("/", async (req, res) => {
+  const { bucketName, data, fileName } = req.body
+
+  await uploadFileToBucket(fileName, bucketName, data)
+  let text =
+    fileName && fileName.endsWith(".pdf")
+      ? await textDetectionFromPdf(fileName, bucketName)
+      : await textDetectionFromImage(fileName, bucketName)
+
+  text = text.replace(/\n/g, " ")
+
+  await deleteFile(fileName, bucketName)
+  return res.status(200).send({ text: text })
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
