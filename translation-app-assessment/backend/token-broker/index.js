d877f995873cc7f52594253b7f4f5cf4693c4b02
maxime_alza <m.alza@alltechconsulting.fr>
2024-06-18 19:09:25 +0200

    Update index.js for token-broker

--- translation-app-assessment/backend/token-broker/index.js
+++ translation-app-assessment/backend/token-broker/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 app.disable('x-powered-by')
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://pole-emploi-trad-dev.firebaseapp.com/',
   credentials: true,  // Permet les requêtes incluant les cookies
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
