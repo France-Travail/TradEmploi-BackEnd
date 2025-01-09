115cdc2d32534fcb8081b4fc5527e2b9ca96c17c
Maxime Alza <maxime.alza-ext@francetravail.fr>
2025-01-09 11:29:39 +0100

    test translation

--- translation-app-assessment/backend/translation/src/deepl.js
+++ translation-app-assessment/backend/translation/src/deepl.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 
 require("dotenv").config()
-const translateWithDeepl = (text, targetLang, sourceLang, targetLangSplitted, sourceLangSplitted, currentUserDomain, isTradTonDoc) => {
+const translateWithDeepl = (text, targetLang, sourceLang, currentUserDomain, isTradTonDoc) => {
     console.log(`Use DeepL cloud translation from ${sourceLang} to ${targetLang} by ${currentUserDomain} for ${isTradTonDoc ? 'TradTonDoc' : 'Chat'}`)
     let glossaryParameter = getGlossaryParameter(sourceLang, targetLang);
     const config = {
@@ -11,34 +11,29 @@ const translateWithDeepl = (text, targetLang, sourceLang, targetLangSplitted, so
         }
     }
 
-    if(targetLang.includes('EN') || targetLang.includes('PT')) {
-        targetLangSplitted = targetLang;
-    }
-
     const data = {
         'text': [text],
         'tag_handling': '',
-        'source_lang': sourceLangSplitted,
-        'target_lang': targetLangSplitted,
+        'source_lang': sourceLang,
+        'target_lang': targetLang,
         'glossary_id': glossaryParameter
     }
-    console.log(data);
     return axios
-    .post(
-      'https://api.deepl.com/v2/translate',
+      .post(
+        'https://api.deepl.com/v2/translate',
         data,
         config
-    )
-    .then((response) => {
-        const texts = [];
-        response.data.translations.forEach(translation => {
-            texts.push(translation.text)
-        })
-        return texts.toString();
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+      )
+      .then((response) => {
+          const texts = [];
+          response.data.translations.forEach(translation => {
+              texts.push(translation.text)
+          })
+          return texts.toString();
+      })
+      .catch((error) => {
+          console.error(error)
+      })
 }
 
 
@@ -48,5 +43,5 @@ function getGlossaryParameter(sourceLang, targetLang) {
 }
 
 module.exports = {
-  translateWithDeepl,
+    translateWithDeepl,
 }
